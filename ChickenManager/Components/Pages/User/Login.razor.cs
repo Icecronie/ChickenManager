beb8d0ee763f50503570698fadc4df1c1ef9f751
Andrew Emrick <icecronie1@gmail.com>
2024-12-27 14:24:03 -0700

    Adding some more base pages with minimal functionality

---
+++ ChickenManager/Components/Pages/User/Login.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace ChickenManager.Components.Pages.User
+{
+    public partial class Login
+    {
+        Models.User User = new Models.User();
+        bool rememberMe = true;
+
+
+        void OnLogin(LoginArgs args, string name)
+        {
+            //console.Log($"{name} -> Username: {args.Username}, password: {args.Password}, remember me: {args.RememberMe}");
+        }
+
+        void OnRegister()
+        {
+            Navigationmanager.NavigateTo("User/RegisterUser");
+        }
+
+        void Reset()
+        {
+            Navigationmanager.NavigateTo("User/ResetPassword");
+        }
+    }
+}
---
+++ ChickenManager/Components/Pages/User/ResetPassword.razor.cs
@@ -0,0 +1,24 @@
+﻿using Radzen;
+
+namespace ChickenManager.Components.Pages.User
+{
+    public partial class ResetPassword
+    {
+        Models.User user = new Models.User();
+        Variant variant = Variant.Filled;
+        bool popup = true;
+
+
+        public async Task Reset(Models.User user)
+        {
+            string sql = "exec CreateNewUser @FirstName, @LastName, @CreatorName, @Email, @PhoneNumber, @Password";
+            object parameters = new
+            {
+                user.Password
+            };
+            var rows = await _data.LoadData<Models.User, dynamic>(sql, parameters, _config.GetConnectionString("Default"));
+
+            //Navigationmanager.NavigateTo("User/RegisterUserSuccess");
+        }
+    }
+}
